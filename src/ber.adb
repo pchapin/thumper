df19bdbce669daac41e782f63def60feba583ede
Peter C. Chapin <PChapin@vtc.vsc.edu>
2013-03-06 13:17:06 -0500

    Enhanced BER package. The subprograms that process the leading identifier octet are now in better shape.

--- src/ber.adb
+++ src/ber.adb
@@ -10,13 +10,14 @@
 
 package body BER is
 
-   function Make_Universal_Tag
+   function Make_Leading_Identifier
      (Tag_Class       : Tag_Class_Type;
       Structured_Flag : Structured_Flag_Type;
-      Tag             : Universal_Tag_Type) return Network.Octet is
+      Tag             : Leading_Number_Type) return Network.Octet is
 
       type Tag_Class_Lookup_Type is array(Tag_Class_Type) of Network.Octet;
       type Structured_Flag_Lookup_Type is array(Structured_Flag_Type) of Network.Octet;
+      type Leading_Number_Lookup_Type is array(Leading_Number_Type) of Network.Octet;
 
       Tag_Class_Lookup_Table : constant Tag_Class_Lookup_Type := Tag_Class_Lookup_Type'
         (Class_Universal        => 2#0000_0000#,
@@ -25,11 +26,159 @@ package body BER is
          Class_Private          => 2#1100_0000#);
 
       Structured_Flag_Lookup_Table : constant Structured_Flag_Lookup_Type := Structured_Flag_Lookup_Type'
-        (Primitive   => 2#0000_0000#,
-         Constructed => 2#0010_0000#);
+        (Primitive              => 2#0000_0000#,
+         Constructed            => 2#0010_0000#);
+
+      Leading_Number_Lookup_Table : constant Leading_Number_Lookup_Type := Leading_Number_Lookup_Type'
+        (Tag_Reserved           =>  0,
+         Tag_Boolean            =>  1,
+         Tag_Integer            =>  2,
+         Tag_Bit_String         =>  3,
+         Tag_Octet_String       =>  4,
+         Tag_Null               =>  5,
+         Tag_Object_Identifier  =>  6,
+         Tag_Object_Descriptor  =>  7,
+         Tag_Instance_Of        =>  8,
+         Tag_External           =>  8,  -- Same as Instance_Of
+         Tag_Real               =>  9,
+         Tag_Enumerated         => 10,
+         Tag_Embedded_PDV       => 11,
+         Tag_UTF8_String        => 12,
+         Tag_Relative_OID       => 13,
+         -- Values 14 .. 15 omitted (not defined?)
+         Tag_Sequence           => 16,
+         Tag_Sequence_Of        => 16,  -- Same as Sequence
+         Tag_Set                => 17,
+         Tag_Set_Of             => 17,  -- Same as Set
+         Tag_Numeric_String     => 18,
+         Tag_Printable_String   => 19,
+         Tag_Teletex_String     => 20,
+         Tag_T61_String         => 20,  -- Same as Teletex_String
+         Tag_Videotex_String    => 21,
+         Tag_IA5_String         => 22,
+         Tag_UTC_Time           => 23,
+         Tag_Generalized_Time   => 24,
+         Tag_Graphic_String     => 25,
+         Tag_Visible_String     => 26,
+         Tag_ISO646_String      => 26,  -- Same as Visible_String
+         Tag_General_String     => 27,
+         Tag_Universal_String   => 28,
+         Tag_Character_String   => 29,
+         Tag_BMP_String         => 30,
+         Tag_EXTENDED_TAG       => 31);
+
 
    begin
-      return Tag_Class_Lookup_Table(Tag_Class) or Structured_Flag_Lookup_Table(Structured_Flag) or Network.Octet(Tag);
-   end Make_Universal_Tag;
+      return
+        Tag_Class_Lookup_Table(Tag_Class)             or
+        Structured_Flag_Lookup_Table(Structured_Flag) or
+        Leading_Number_Lookup_Table(Tag);
+   end Make_Leading_Identifier;
+
+
+   procedure Split_Leading_Identifier
+     (Value           : in  Network.Octet;
+      Tag_Class       : out Tag_Class_Type;
+      Structured_Flag : out Structured_Flag_Type;
+      Tag             : out Leading_Number_Type;
+      Status          : out Status_Type) is
+
+      subtype Leading_Number_Range_Type is Network.Octet range 0 .. 31;
+      type Leading_Number_Lookup_Type is array(Leading_Number_Range_Type) of Leading_Number_Type;
+      Leading_Number_Lookup_Table : constant Leading_Number_Lookup_Type := Leading_Number_Lookup_Type'
+        ( 0 => Tag_Reserved,
+          1 => Tag_Boolean,
+          2 => Tag_Integer,
+          3 => Tag_Bit_String,
+          4 => Tag_Octet_String,
+          5 => Tag_Null,
+          6 => Tag_Object_Identifier,
+          7 => Tag_Object_Descriptor,
+          8 => Tag_Instance_Of,        -- Could also be Tag_External
+          9 => Tag_Real,
+         10 => Tag_Enumerated,
+         11 => Tag_Embedded_PDV,
+         12 => Tag_UTF8_String,
+         13 => Tag_Relative_OID,
+         14 => Tag_Null,               -- 14 is undefined.
+         15 => Tag_Null,               -- 15 is undefined.
+         16 => Tag_Sequence,           -- Could also be Tag_Sequence_Of
+         17 => Tag_Set,                -- Could also be Tag_Set_Of
+         18 => Tag_Numeric_String,
+         19 => Tag_Printable_String,
+         20 => Tag_Teletex_String,     -- Could also be Tag_T61_String
+         21 => Tag_Videotex_String,
+         22 => Tag_IA5_String,
+         23 => Tag_UTC_Time,
+         24 => Tag_Generalized_Time,
+         25 => Tag_Graphic_String,
+         26 => Tag_Visible_String,     -- Could also be Tag_ISO646_String
+         27 => Tag_General_String,
+         28 => Tag_Universal_String,
+         29 => Tag_Character_String,
+         30 => Tag_BMP_String,
+         31 => Tag_EXTENDED_TAG);
+
+      procedure Set_Tag_Class
+      --# global in Value; out Tag_Class;
+      --# derives Tag_Class from Value;
+      is
+      begin
+         -- Deal with the class.
+         case (Value and 2#1100_0000#) is
+            when 2#0000_0000# => Tag_Class := Class_Universal;
+            when 2#0100_0000# => Tag_Class := Class_Application;
+            when 2#1000_0000# => Tag_Class := Class_Context_Specific;
+            when 2#1100_0000# => Tag_Class := Class_Private;
+            when others => Tag_Class := Class_Universal;   -- Should never happen.
+         end case;
+      end Set_Tag_Class;
+
+      procedure Set_Structured_Flag
+      --# global in Value; out Structured_Flag;
+      --# derives Structured_Flag from Value;
+      is
+      begin
+         -- Deal with the structured flag.
+         case (Value and 2#0010_0000#) is
+            when 2#0000_0000# => Structured_Flag := Primitive;
+            when 2#0010_0000# => Structured_Flag := Constructed;
+            when others => Structured_Flag := Primitive;   -- Should never happen.
+         end case;
+      end Set_Structured_Flag;
+
+      procedure Set_Tag
+      --# global in Value; out Tag; in out Status;
+      --# derives Tag    from Value &
+      --#         Status from Value, Status;
+      is
+         Tag_Value : Leading_Number_Range_Type;
+      begin
+         -- Deal with the tag.
+         Tag_Value := (Value and 2#0001_1111#);
+         if Tag_Value = 14 or Tag_Value = 15 then
+            Status := Bad_Identifier;
+         end if;
+         Tag := Leading_Number_Lookup_Table(Tag_Value);
+      end Set_Tag;
+
+   begin
+      Status := Success;
+
+      Set_Tag_Class;
+      Set_Structured_Flag;
+      Set_Tag;
+   end Split_Leading_Identifier;
+
+
+   procedure Get_Length_Value
+     (Message : in  Network.Octet_Array;
+      Offset  : in  Natural;
+      Stop    : out Natural;
+      Length  : out Natural;
+      Status  : out Status_Type) is
+   begin
+      null;
+   end Get_Length_Value;
 
 end BER;
--- src/ber.ads
+++ src/ber.ads
@@ -13,51 +13,73 @@ use type Network.Octet;
 --# inherit Network;
 package BER is
 
+   type Status_Type is (Success, Bad_Identifier, Indefinite_Length, Unimplemented_Length, Bad_Length);
+
    type Tag_Class_Type is (Class_Universal, Class_Application, Class_Context_Specific, Class_Private);
    type Structured_Flag_Type is (Primitive, Constructed);
+   type Leading_Number_Type is
+     (Tag_Reserved,
+      Tag_Boolean,
+      Tag_Integer,
+      Tag_Bit_String,
+      Tag_Octet_String,
+      Tag_Null,
+      Tag_Object_Identifier,
+      Tag_Object_Descriptor,
+      Tag_Instance_Of,
+      Tag_External,
+      Tag_Real,
+      Tag_Enumerated,
+      Tag_Embedded_PDV,
+      Tag_UTF8_String,
+      Tag_Relative_OID,
+      Tag_Sequence,
+      Tag_Sequence_Of,
+      Tag_Set,
+      Tag_Set_Of,
+      Tag_Numeric_String,
+      Tag_Printable_String,
+      Tag_Teletex_String,
+      Tag_T61_String,
+      Tag_Videotex_String,
+      Tag_IA5_String,
+      Tag_UTC_Time,
+      Tag_Generalized_Time,
+      Tag_Graphic_String,
+      Tag_Visible_String,
+      Tag_ISO646_String,
+      Tag_General_String,
+      Tag_Universal_String,
+      Tag_Character_String,
+      Tag_BMP_String,
+      Tag_EXTENDED_TAG);
 
-   -- Universal tags.
-   type Universal_Tag_Type is range 0 .. 31;
-
-   Tag_Reserved           : constant Universal_Tag_Type := 0;
-   Tag_Boolean            : constant Universal_Tag_Type := 1;
-   Tag_Integer            : constant Universal_Tag_Type := 2;
-   Tag_Bit_String         : constant Universal_Tag_Type := 3;
-   Tag_Octet_String       : constant Universal_Tag_Type := 4;
-   Tag_Null               : constant Universal_Tag_Type := 5;
-   Tag_Object_Identifier  : constant Universal_Tag_Type := 6;
-   Tag_Object_Descriptor  : constant Universal_Tag_Type := 7;
-   Tag_Instance_Of        : constant Universal_Tag_Type := 8;
-   Tag_External           : constant Universal_Tag_Type := 8;  -- Same as Instance_Of
-   Tag_Real               : constant Universal_Tag_Type := 9;
-   Tag_Enumerated         : constant Universal_Tag_Type := 10;
-   Tag_Embedded_PDV       : constant Universal_Tag_Type := 11;
-   Tag_UTF8_String        : constant Universal_Tag_Type := 12;
-   Tag_Relative_OID       : constant Universal_Tag_Type := 13;
-   -- Values 14 .. 15 omitted (not defined?)
-   Tag_Sequence           : constant Universal_Tag_Type := 16;
-   Tag_Sequence_Of        : constant Universal_Tag_Type := 16;  -- Same as Sequence
-   Tag_Set                : constant Universal_Tag_Type := 17;
-   Tag_Set_Of             : constant Universal_Tag_Type := 17;  -- Same as Set
-   Tag_Numeric_String     : constant Universal_Tag_Type := 18;
-   Tag_Printable_String   : constant Universal_Tag_Type := 19;
-   Tag_Teletex_String     : constant Universal_Tag_Type := 20;
-   Tag_T61_String         : constant Universal_Tag_Type := 20;  -- Same as Teletex_String
-   Tag_Videotex_String    : constant Universal_Tag_Type := 21;
-   Tag_IA5_String         : constant Universal_Tag_Type := 22;
-   Tag_UTC_Time           : constant Universal_Tag_Type := 23;
-   Tag_Generalized_Time   : constant Universal_Tag_Type := 24;
-   Tag_Graphic_String     : constant Universal_Tag_Type := 25;
-   Tag_Visible_String     : constant Universal_Tag_Type := 26;
-   Tag_ISO646_String      : constant Universal_Tag_Type := 26;  -- Same as Visible_String
-   Tag_General_String     : constant Universal_Tag_Type := 27;
-   Tag_Universal_String   : constant Universal_Tag_Type := 28;
-   Tag_Character_String   : constant Universal_Tag_Type := 29;
-   Tag_BMP_String         : constant Universal_Tag_Type := 30;
-   Tag_EXTENDED_TAG       : constant Universal_Tag_Type := 31;
-
-   function Make_Universal_Tag
+   function Make_Leading_Identifier
      (Tag_Class       : Tag_Class_Type;
       Structured_Flag : Structured_Flag_Type;
-      Tag             : Universal_Tag_Type) return Network.Octet;
+      Tag             : Leading_Number_Type) return Network.Octet;
+
+   procedure Split_Leading_Identifier
+     (Value           : in  Network.Octet;
+      Tag_Class       : out Tag_Class_Type;
+      Structured_Flag : out Structured_Flag_Type;
+      Tag             : out Leading_Number_Type;
+      Status          : out Status_Type);
+   --# derives Tag_Class, Structured_Flag, Tag, Status from Value;
+
+   -- Examines the Message starting at position Offset looking for a BER encoded length. That length is returned in Length.
+   -- The index of the last octet processed as part of the encoded length is returned in Stop.
+   --  "Fails" with Indefinite_Length if the encoded length is in the indefinite form. In that case the returned value of
+   --     Stop points at the last octet of the encoded length as usual and the returned length is zero.
+   --   Fails with Bad_Length if the data is not encoded properly. In that case the returned length is zero.
+   --   Fails with Unimplemented_Length if the encoded length is too large for this implementation to handle. In that case
+   --     the returned value of Stop points at the last octet of the full length.
+   --
+   procedure Get_Length_Value
+     (Message : in  Network.Octet_Array;
+      Offset  : in  Natural;
+      Stop    : out Natural;
+      Length  : out Natural;
+      Status  : out Status_Type);
+   --# derives Stop, Length, Status from Message, Offset;
 end BER;
