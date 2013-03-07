0afce6126b41eb65421d0df7f9fde619f38bbca2
Peter C. Chapin <PChapin@vtc.vsc.edu>
2013-03-07 18:21:28 -0500

    Implemented the Serial_Generator package.

--- src/serial_generator.adb
+++ src/serial_generator.adb
@@ -13,14 +13,49 @@ package body Serial_Generator is
    Current_Number : Serial_Number_Type;
 
    procedure Initialize(Status : out Status_Type) is
+      Number_File        : SPARK_IO.File_Type;
+      Number_File_Status : SPARK_IO.File_Status;
+      Raw_Number         : Integer;  -- TODO: We should really read Serial_Number_Type values from the file.
+      Read_Success       : Boolean;
    begin
+      Current_Number := 0;
       Status := Bad_Number;
+      SPARK_IO.Open(Number_File, SPARK_IO.In_File, "serial-number.txt", "", Number_File_Status);
+      if Number_File_Status = SPARK_IO.Ok then
+         SPARK_IO.Get_Integer(Number_File, Raw_Number, 0, Read_Success);
+         if Read_Success and then Raw_Number >= 0 then
+            Current_Number := Serial_Number_Type(Raw_Number);
+            Status := Success;
+         end if;
+         --# accept flow_message, 10, Number_File, "Close sets Number_File to an invalid value";
+         --# accept flow_message, 10, Number_File_Status, "Failure when closing an input file has no bad effects";
+         SPARK_IO.Close(Number_File, Number_File_Status);
+         --# end accept;
+         --# end accept;
+      end if;
    end Initialize;
 
 
-   procedure Advance is
+   procedure Advance(Status : out Status_Type) is
+      Number_File        : SPARK_IO.File_Type;
+      Number_File_Status : SPARK_IO.File_Status;
+      Raw_Number         : Integer;  -- TODO: We should really write Serial_Number_Type values to the file.
    begin
-      null;
+      Status := Bad_Update;
+      if Current_Number /= Serial_Number_Type'Last then
+         Current_Number := Current_Number + 1;
+         SPARK_IO.Open(Number_File, SPARK_IO.Out_File, "serial-number.txt", "", Number_File_Status);
+         if Number_File_Status = SPARK_IO.Ok and then Current_Number <= Serial_Number_Type(Integer'Last) then
+            Raw_Number := Integer(Current_Number);
+            SPARK_IO.Put_Integer(Number_File, Raw_Number, 0, 10);
+            --# accept flow_message, 10, Number_File, "Close sets Number_File to an invalid value";
+            SPARK_IO.Close(Number_File, Number_File_Status);
+            --# end accept;
+            if Number_File_Status = SPARK_IO.Ok then
+               Status := Success;
+            end if;
+         end if;
+      end if;
    end Advance;
 
 
--- src/serial_generator.ads
+++ src/serial_generator.ads
@@ -3,18 +3,22 @@
 -- SUBJECT : Specification of a package to abstract the serial number generator.
 -- AUTHOR  : (C) Copyright 2013 by Peter Chapin and John McCormick
 --
+-- Serial numbers are required to be unique over the lifetime of the system's deployment.
+--
 -- Please send comments or bug reports to
 --
 --      Peter Chapin <PChapin@vtc.vsc.edu>
 ---------------------------------------------------------------------------
 with SPARK_IO;
 
+use type SPARK_IO.File_Status;
+
 --# inherit SPARK_IO;
 package Serial_Generator
 --# own Current_Number;
 is
    type Serial_Number_Type is mod 2**64;
-   type Status_Type is (Success, Bad_Number);
+   type Status_Type is (Success, Bad_Number, Bad_Update);
 
    -- Initializes the current number. Fails with Bad_Number if an appropriate value couldn't be read from the file system.
    procedure Initialize(Status : out Status_Type);
@@ -22,14 +26,15 @@ is
    --# derives Current_Number  from SPARK_IO.State, SPARK_IO.Inputs &
    --#         Status          from SPARK_IO.State, SPARK_IO.Inputs &
    --#         SPARK_IO.State  from SPARK_IO.State &
-   --#         SPARK_IO.Inputs from SPARK_IO.Inputs;
+   --#         SPARK_IO.Inputs from SPARK_IO.State, SPARK_IO.Inputs;
 
-   -- Computes (and saves) the next serial number. The serial numbers are to be unique over the lifetime of the deployment.
-   procedure Advance;
+   -- Computes (and saves) the next serial number. Fails with Bad_Update if a new serial number couldn't be calculated.
+   procedure Advance(Status : out Status_Type);
    --# global in out Current_Number; in out SPARK_IO.State, SPARK_IO.Outputs;
    --# derives Current_Number   from Current_Number &
-   --#         SPARK_IO.State   from SPARK_IO.State &
-   --#         SPARK_IO.Outputs from Current_Number, SPARK_IO.Outputs;
+   --#         Status           from Current_Number, SPARK_IO.State &
+   --#         SPARK_IO.State   from Current_Number, SPARK_IO.State &
+   --#         SPARK_IO.Outputs from Current_Number, SPARK_IO.State, SPARK_IO.Outputs;
 
    -- Returns the current serial number.
    function Get return Serial_Number_Type;
