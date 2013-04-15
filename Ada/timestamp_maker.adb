df51020500cde832d2d0bcb12e7d198874cdf99b
Peter Chapin <PChapin@vtc.vsc.edu>
2013-04-15 14:38:23 -0400

    Sketched Homework #8.

--- Ada/timestamp_maker.adb
+++ Ada/timestamp_maker.adb
@@ -10,9 +10,40 @@
 
 package body Timestamp_Maker is
 
+   type Imprint is
+      record
+         X : Integer; -- TODO: Replace with appropriate content.
+      end record;
+
+   -- Extracts the message imprint from the request. The meaning of the parameters are as follows.
+   --   Request_Message : The full message array.
+   --   Request_Count   : The number of bytes in Request_Message that are meaningful.
+   --   Index           : The first position in Request_Message where the imprint starts.
+   --   Message_Imprint : The imprint obtained from the message.
+   --   Stop            : The last position of the imprint.
+   --   Message_Status  : Indicates the success or failure of imprint extraction.
+   --
+   procedure Get_Message_Imprint
+     (Request_Message : in  Messages.Message;
+      Request_Count   : in  Messages.Count_Type;
+      Index           : in  Messages.Index_Type;
+      Message_Imprint : out Imprint;
+      Stop            : out Messages.Index_Type;
+      Imprint_Status  : out Status_Type) is
+   begin
+      null;
+   end Get_Message_Imprint;
+
+
    function Valid_Request(Request_Message : in Messages.Message; Request_Count : in Messages.Count_Type) return Boolean is
    begin
-      return False;
+      -- This is Homework #8:
+      -- Check that the first octet in Request_Message is 16#30# (a sequence).
+      -- Get the sequence length. For now, treat indefinite lengths as errors.
+      -- Compare sequence length to Request_Count. Return False if request incomplete or extra junk present.
+      -- Get the first sequence item (version) as an Integer. Check that it is version 1.
+      -- Get the message imprint from Request_Message and verify that doing so was successful.
+      return True;
    end Valid_Request;
 
 
