fbd0d077cc671b5eab7f350296daf24fbd08cb27
Peter C. Chapin <PChapin@vtc.vsc.edu>
2014-12-12 12:02:42 -0500

    Changed the serial number generator to use a linear congruential PRNG instead of storing sequential serial numbers in a file.

--- src/serial_generator.adb
+++ src/serial_generator.adb
@@ -7,50 +7,47 @@
 --
 --      Peter Chapin <PChapin@vtc.vsc.edu>
 ---------------------------------------------------------------------------
-pragma SPARK_Mode(Off);
+pragma SPARK_Mode(On);
+with Ada.Calendar;
 
-with Ada.Text_IO;
+package body Serial_Generator
+  with Refined_State => (State => Current)
+is
 
-package body Serial_Generator is
+   -- See: http://nuclear.llnl.gov/CNP/rng/rngman/node4.html
+   -- This generator has the maximal period.
+   A : constant Serial_Number_Type := 2_862_933_555_777_941_757;
+   B : constant Serial_Number_Type := 3_037_000_493;
 
-   package Serial_Number_IO is new Ada.Text_IO.Modular_IO(Serial_Generator.Serial_Number_Type);
+   Current : Serial_Number_Type;
 
-   Current_Number : Serial_Number_Type;
-
-   procedure Initialize(Status : out Status_Type) is
-      Number_File : Ada.Text_IO.File_Type;
+   procedure Next(Number : out Serial_Number_Type)
+     with
+        Refined_Global => (In_Out => Current),
+        Refined_Depends => ((Current, Number) => Current)
+   is
    begin
-      Current_Number := 0;
-      Status := Success;
-      Ada.Text_IO.Open(Number_File, Ada.Text_IO.In_File, "serial-number.txt");
-      Serial_Number_IO.Get(Number_File, Current_Number);
-      Ada.Text_IO.Close(Number_File);
-   exception
-      when others =>
-         Status := Bad_Number;
-   end Initialize;
+      Current := A * Current + B;
+      Number  := Current;
+   end Next;
 
-
-   procedure Advance(Status : out Status_Type) is
-      Number_File : Ada.Text_IO.File_Type;
+begin
+   declare
+      use Ada.Calendar;
+      Year    : Year_Number;
+      Month   : Month_Number;
+      Day     : Day_Number;
+      Seconds : Day_Duration;
    begin
-      Status := Bad_Update;
-      if Current_Number /= Serial_Number_Type'Last then
-         Status := Success;
-         Current_Number := Current_Number + 1;
-         Ada.Text_IO.Open(Number_File, Ada.Text_IO.Out_File, "serial-number.txt");
-         Serial_Number_IO.Put(Number_File, Current_Number);
-         Ada.Text_IO.Close(Number_File);
-      end if;
-   exception
-      when others =>
-         Status := Bad_Update;
-   end Advance;
-
-
-   function Get return Serial_Number_Type is
-   begin
-      return Current_Number;
-   end Get;
-
+      -- Clock does read a global time. However, it's not worth modeling fully here.
+      pragma Warnings(Off, "no Global contract available for ""Split""");
+      pragma Warnings(Off, "no Global contract available for ""Clock""");
+      Split(Clock, Year, Month, Day, Seconds);
+      -- Store the date/time into a 64 bit value. The precise layout is not important.
+      Current :=
+        Serial_Number_Type(Year - Year_Number'First) * 2**54 +  -- 9 bits.
+        Serial_Number_Type(  Month) * 2**50 +  -- 4 bits.
+        Serial_Number_Type(    Day) * 2**45 +  -- 5 bits.
+        Serial_Number_Type(Seconds);
+   end;
 end Serial_Generator;
--- src/serial_generator.ads
+++ src/serial_generator.ads
@@ -12,30 +12,20 @@
 pragma SPARK_Mode(On);
 
 package Serial_Generator
-with
-  Abstract_State => Number
+  with
+     Abstract_State => State,
+     Initializes => State
 is
    type Serial_Number_Type is mod 2**64;
-   type Status_Type is (Success, Bad_Number, Bad_Update);
 
-   -- Initializes the current number. Fails with Bad_Number if an appropriate value couldn't be
-   -- read from the file system.
-   procedure Initialize(Status : out Status_Type)
-   with
-     Global => (Output => Number),
-     Depends => ((Number, Status) => null);
-
-   -- Computes (and saves) the next serial number. Fails with Bad_Update if a new serial number
-   -- couldn't be saved or if the serial number is already at its maximum value and can't be
-   -- advanced.
-   procedure Advance(Status : out Status_Type)
-   with
-     Global => (In_Out => Number),
-     Depends => ((Number, Status) => Number);
-
-   -- Returns the current serial number.
-   function Get return Serial_Number_Type
-   with
-     Global => (Input => Number);
+   -- Computes the next serial number and updates the internal state to prepare for a following
+   -- call to Next. This procedure can't fail. However, the numbers it generates cycle with a
+   -- period of 2**64.
+   --
+   procedure Next(Number : out Serial_Number_Type)
+     with
+        Global => (In_Out => State),
+        Depends => ((State, Number) => State);
 
 end Serial_Generator;
+
