77cd97d90cf5beaeed5463e535475b77cbe0691a
Peter C. Chapin <peter@pchapin.org>
2022-05-16 14:08:28 -0400

    Finished cosmetic cleanup in data_storage.adb.

--- src/server/data_storage.adb
+++ src/server/data_storage.adb
@@ -11,7 +11,6 @@
 -- TODO: Remove dependency on Ada.Text_IO. Use a real logger.
 with Ada.Calendar.Formatting;
 with Ada.Calendar.Time_Zones;
-with Ada.Text_IO;
 
 with Hermes.OID;
 with Postgresql;
@@ -42,6 +41,61 @@ package body Data_Storage is
       -- TODO: Time zones need to be formatted to match Generalized_Time, right now they are
       -- returning the second character in a string representing the UFC Time Offset
    end Time_To_GeneralizedTime;
+
+
+   function Process_Tuples(Number_Of_Tuples : in Count_Type) return Timestamp_Array is
+      Version              : Positive;
+      Policy_String        : String(1 .. 128);
+      Policy_Components    : Hermes.OID.Component_Array(1 .. Hermes.OID.Maximum_Component_Count);
+      Policy_OID           : Hermes.OID.Object_Identifier;
+      Policy_Status        : Hermes.OID.Status_Type;
+      Algorithm_String     : String(1 .. 128);
+      Algorithm_Components : Hermes.OID.Component_Array(1 .. Hermes.OID.Maximum_Component_Count);
+      Algorithm_OID        : Hermes.OID.Object_Identifier;
+      Algorithm_Status     : Hermes.OID.Status_Type;
+      Hash_String          : String(1 .. 96);
+      Hash_Octets          : Hermes.Octet_Array(1 .. Timestamp_Messages.Hash_Size);
+      Serial_Number        : Serial_Number_Type;
+      Time_String          : String(1 .. 15);
+
+      Result : Timestamp_Array(1 .. Number_Of_Tuples);
+   begin
+      -- TODO: This code assumes the strings all have the same size, which isn't really true.
+      for I in 1 .. Number_Of_Tuples loop
+         Version := Positive'Value(PostgreSQL.Get_Value((I - 1), 0));
+
+         -- TODO: Check the status value from To_Object_Identifier.
+         Policy_String := PostgreSQL.Get_Value((I - 1), 1);
+         Hermes.OID.String_To_OID(Policy_String, Policy_Components);
+         Hermes.OID.To_Object_Identifier
+           (Separates => Policy_Components,
+            Result    => Policy_OID,
+            Status    => Policy_Status);
+
+         -- TODO: Check the status value from To_Object_Identifier.
+         Algorithm_String := PostgreSQL.Get_Value((I - 1), 2);
+         Hermes.OID.String_To_OID(Algorithm_String, Algorithm_Components);
+         Hermes.OID.To_Object_Identifier
+           (Separates => Algorithm_Components,
+            Result    => Algorithm_OID,
+            Status    => Algorithm_Status);
+
+         Hash_String := PostgreSQL.Get_Value((I - 1), 3);
+         Hermes.String_To_Octet_Array(Hash_String, Hash_Octets);
+
+         Serial_Number := Serial_Number_Type'Value(PostgreSQL.Get_Value((I - 1), 4));
+         Time_String := PostgreSQL.Get_Value((I - 1), 5);
+
+         Result(I).Version          := Version;
+         Result(I).Policy           := Policy_OID;
+         Result(I).Hash_Algorithm   := Algorithm_OID;
+         Result(I).Hashed_Message   := Hash_Octets;
+         Result(I).Serial_Number    := Serial_Number;
+         Result(I).Generalized_Time := Time_String;
+      end loop;
+      return Result;
+   end Process_Tuples;
+
 
    ----------------------
    -- Public Subprograms
@@ -107,147 +161,43 @@ package body Data_Storage is
            Positive'Image(Stamp.Version) & "', '" &
            Policy_String                 & "', '" &
            Hash_Algorithm_String         & "', '" &
-           Hash_String                   & "', " &
+           Hash_String                   & "', "  &
            Serial_Number_Type'Image(Stamp.Serial_Number) & ", '" &
            Stamp.Generalized_Time        & "');");
    end Timestamp_Store;
 
 
    function Timestamp_Retrieve(Serial_Number : Serial_Number_Type) return Timestamp_Array is
-      Tuples : Count_Type;
    begin
-      PostgreSQL.Execute_Query(Query => "SELECT Version, Policy, Hash_Algorithm, Hash_Message, Serial_Number, Generalized_Time FROM ThumperTable WHERE Serial_Number = " & Serial_Number_Type'Image(Serial_Number) & ";");
-      Tuples := PostgreSQL.Number_Of_Tuples;
-      declare
-         Result : Timestamp_Array(1 .. Tuples);
-      begin
-         if Tuples >= 1 then
-            for I in 1 .. Tuples loop
-            declare
-               Iversion    : Positive;                                  -- Version
-               Spolicy     : String(1..128);                            -- Policy
-               Cpolicy     : Hermes.OID.Component_Array(1..Hermes.OID.Maximum_Component_Count);
-               OIDpolicy   : Hermes.OID.Object_Identifier;
-               Stpolicy    : Hermes.OID.Status_Type;
-               Salgo       : String(1..128);                            -- Hash_Algorithm
-               Calgo       : Hermes.OID.Component_Array(1..Hermes.OID.Maximum_Component_Count);
-               OIDalgo     : Hermes.OID.Object_Identifier;
-               Stalgo      : Hermes.OID.Status_Type;
-               Smess       : String(1..96);                             -- Hash_Message
-               Omess       : Hermes.Octet_Array(1..Timestamp_Messages.Hash_Size);
-               Snum        : Serial_Number_Type;                        -- Serial_Number
-               Stime       : String(1..15);                             -- Generalized_Time
-            begin
-               Iversion := Positive'Value(PostgreSQL.Get_Value ((I - 1),0));
-                  Ada.Text_IO.Put_Line ("Version: " & Iversion'Image);
-                  Ada.Text_IO.Put_Line("");
-               Spolicy := PostgreSQL.Get_Value ((I - 1), 1);
-                  Ada.Text_IO.Put_Line (Item => "Policy: " & Spolicy);
-               Hermes.OID.String_To_OID(Spolicy, Cpolicy);
-               Hermes.OID.To_Object_Identifier(Separates => Cpolicy, Result => OIDpolicy, Status => Stpolicy);
-                  Ada.Text_IO.Put_Line (Item => "OID Status: " & Hermes.OID.Status_Type'Image(Stpolicy));
-                  Ada.Text_IO.Put_Line("");
-               Salgo := PostgreSQL.Get_Value ((I - 1), 2);
-                  Ada.Text_IO.Put_Line (Item => "Hash Algorithm: " & Salgo);
-               Hermes.OID.String_To_OID(Salgo, Calgo);
-               Hermes.OID.To_Object_Identifier(Separates => Calgo, Result => OIDalgo, Status => Stalgo);
-                  Ada.Text_IO.Put_Line ("OID Status: " & Hermes.OID.Status_Type'Image(Stalgo));
-                  Ada.Text_IO.Put_Line("");
-               Smess := PostgreSQL.Get_Value ((I - 1), 3);
-                  Ada.Text_IO.Put_Line (Item => "Hashed Message: " & Smess);
-                  Ada.Text_IO.Put_Line("");
-               Hermes.String_To_Octet_Array (Smess, Omess);
-               Snum := Serial_Number_Type'Value(PostgreSQL.Get_Value ((I - 1), 4));
-                  Ada.Text_IO.Put_Line (Item => "Serial Number: " & Serial_Number_Type'Image(Snum));
-                  Ada.Text_IO.Put_Line("");
-               Stime := PostgreSQL.Get_Value (Tuple_Number => (I - 1), Field_Number => 5);
-                  Ada.Text_IO.Put_Line (Item => "Generalized Time: " & Stime);
-                  Ada.Text_IO.Put_Line("");
-               Result(I).Version          := Iversion;
-               Result(I).Policy           := OIDpolicy;
-               Result(I).Hash_Algorithm   := OIDalgo;
-               Result(I).Hashed_Message   := Omess;
-               Result(I).Serial_Number    := Snum;
-               Result(I).Generalized_Time := Stime;
-            end;
-         end loop;
-         else
-            raise Program_Error with "Invalid Serial Number: not assigned.";
-         end if;
-         return Result;
-      end;
+      PostgreSQL.Execute_Query
+        (Query => "SELECT version, "         &
+                         "policy, "          &
+                         "hash_algorithm, "  &
+                         "hash, "            &
+                         "serial_number, "   &
+                         "generalized_time " &
+                  "FROM  timestamp "         &
+                  "WHERE serial_number = " & Serial_Number_Type'Image(Serial_Number) & ";");
+      return Process_Tuples(PostgreSQL.Number_Of_Tuples);
    end Timestamp_Retrieve;
 
 
    function Timestamp_Retrieve(Start : Time; Stop : Time) return Timestamp_Array is
-      Tuples   : Count_Type;
-      StartS   : String(1..15);
-      StopS    : String(1..15);
+      Start_Time_String : String(1 .. 15);
+      Stop_Time_String  : String(1 .. 15);
    begin
-      Time_To_GeneralizedTime (Start, StartS);
-      Time_To_GeneralizedTime (Stop, StopS);
-      if StartS > StopS then
-         raise Program_Error with "Invalid Times: start time comes after stop time.";
-      end if;
-      PostgreSQL.Execute_Query(Query => "SELECT Version, Policy, Hash_Algorithm, Hash_Message, Serial_Number, Generalized_Time FROM ThumperTable WHERE Generalized_Time BETWEEN '" & StartS & "' AND '" & StopS & "';");
-      Tuples := PostgreSQL.Number_Of_Tuples;
-      declare
-         Result   : Timestamp_Array(1 .. Tuples);
-      begin
-         if Tuples = 0 then
-            raise Program_Error with "No Timestamps between" & Ada.Calendar.Formatting.Image(Start) & " and " & Ada.Calendar.Formatting.Image(Stop) & ".";
-         end if;
-         for I in 1 .. Tuples loop
-            declare
-               Iversion    : Positive;                                  -- Version
-               Spolicy     : String(1..128);                            -- Policy
-               Cpolicy     : Hermes.OID.Component_Array(1..Hermes.OID.Maximum_Component_Count);
-               OIDpolicy   : Hermes.OID.Object_Identifier;
-               Stpolicy    : Hermes.OID.Status_Type;
-               Salgo       : String(1..128);                            -- Hash_Algorithm
-               Calgo       : Hermes.OID.Component_Array(1..Hermes.OID.Maximum_Component_Count);
-               OIDalgo     : Hermes.OID.Object_Identifier;
-               Stalgo      : Hermes.OID.Status_Type;
-               Smess       : String(1..96);                             -- Hash_Message
-               Omess       : Hermes.Octet_Array(1..Timestamp_Messages.Hash_Size);
-               Snum        : Serial_Number_Type;                        -- Serial_Number
-               Stime       : String(1..15);                             -- Generalized_Time
-            begin
-               Iversion := Positive'Value(PostgreSQL.Get_Value ((I - 1),0));
-                  Ada.Text_IO.Put_Line ("Version: " & Iversion'Image);
-                  Ada.Text_IO.Put_Line("");
-               Spolicy := PostgreSQL.Get_Value ((I - 1), 1);
-                  Ada.Text_IO.Put_Line (Item => "Policy: " & Spolicy);
-               Hermes.OID.String_To_OID(Spolicy, Cpolicy);
-               Hermes.OID.To_Object_Identifier(Separates => Cpolicy, Result => OIDpolicy, Status => Stpolicy);
-                  Ada.Text_IO.Put_Line (Item => "OID Status: " & Hermes.OID.Status_Type'Image(Stpolicy));
-                  Ada.Text_IO.Put_Line("");
-               Salgo := PostgreSQL.Get_Value ((I - 1), 2);
-                  Ada.Text_IO.Put_Line (Item => "Hash Algorithm: " & Salgo);
-               Hermes.OID.String_To_OID(Salgo, Calgo);
-               Hermes.OID.To_Object_Identifier(Separates => Calgo, Result => OIDalgo, Status => Stalgo);
-                  Ada.Text_IO.Put_Line ("OID Status: " & Hermes.OID.Status_Type'Image(Stalgo));
-                  Ada.Text_IO.Put_Line("");
-               Smess := PostgreSQL.Get_Value ((I - 1), 3);
-                  Ada.Text_IO.Put_Line (Item => "Hashed Message: " & Smess);
-                  Ada.Text_IO.Put_Line("");
-               Hermes.String_To_Octet_Array (Smess, Omess);
-               Snum := Serial_Number_Type'Value(PostgreSQL.Get_Value ((I - 1), 4));
-                  Ada.Text_IO.Put_Line (Item => "Serial Number: " & Serial_Number_Type'Image(Snum));
-                  Ada.Text_IO.Put_Line("");
-               Stime := PostgreSQL.Get_Value (Tuple_Number => (I - 1), Field_Number => 5);
-                  Ada.Text_IO.Put_Line (Item => "Generalized Time: " & Stime);
-                  Ada.Text_IO.Put_Line("");
-               Result(I).Version          := Iversion;
-               Result(I).Policy           := OIDpolicy;
-               Result(I).Hash_Algorithm   := OIDalgo;
-               Result(I).Hashed_Message   := Omess;
-               Result(I).Serial_Number    := Snum;
-               Result(I).Generalized_Time := Stime;
-            end;
-         end loop;
-      return Result;
-      end;
+      Time_To_GeneralizedTime(Start, Start_Time_String);
+      Time_To_GeneralizedTime(Stop, Stop_Time_String);
+      PostgreSQL.Execute_Query
+        (Query => "SELECT version, "         &
+                         "policy, "          &
+                         "hash_algorithm, "  &
+                         "hash, "            &
+                         "serial_number, "   &
+                         "generalized_time " &
+                  "FROM  timestamp "         &
+                  "WHERE generalized_time BETWEEN '" & Start_Time_String & "' AND '" & Stop_Time_String & "';");
+      return Process_Tuples(PostgreSQL.Number_Of_Tuples);
    end Timestamp_Retrieve;
 
 end Data_Storage;
